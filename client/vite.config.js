98f60821ba1cbfcc32a25a5ae82fe470f7a27740
Abhishek Singh Chauhan <cbittu349@gmail.com>
2025-09-01 01:15:03 +0530

    new changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,47 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react() ,   tailwindcss(),],
-})
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          50: "#eff6ff",
+          100: "#dbeafe",
+          200: "#bfdbfe",
+          300: "#93c5fd",
+          400: "#60a5fa",
+          500: "#3b82f6",
+          600: "#2563eb", // bg-primary-600 will now work
+          700: "#1d4ed8",
+          800: "#1e40af",
+          900: "#1e3a8a",
+        },
+        secondary: {
+          100: "#fce7f3",
+          200: "#fbcfe8",
+          400: "#f472b6",
+          600: "#db2777",
+          800: "#9d174d",
+        },
+        success: {
+          500: "#22c55e",
+          600: "#16a34a",
+        },
+        warning: {
+          500: "#f59e0b",
+          600: "#d97706",
+        },
+        danger: {
+          500: "#ef4444",
+          600: "#dc2626",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
